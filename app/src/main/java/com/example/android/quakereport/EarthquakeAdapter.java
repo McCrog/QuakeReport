2404c7469008d99b77808d3ce623cc93f5126f67
Alexander <pit_mib@bk.ru>
2017-10-30 21:58:26 +0300

    Added some visual polish on the list

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,8 +1,10 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,17 +57,27 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         // Display the magnitude of the current earthquake in that TextView
         magnitudeTextView.setText(formattedMagnitude);
 
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
         String currentLocation = currentEarthquake.getLocation();
 
         // Find the TextView in the list_item.xml layout with the ID offset.
-        TextView offsetTextView = (TextView) listItemView.findViewById(R.id.offset);
+        TextView offsetTextView = (TextView) listItemView.findViewById(R.id.location_offset);
         // Get the location from the formatLocation method and set this text on
         // the TextView.
         // 0 index is a location offset string
         offsetTextView.setText(formatLocation(currentLocation)[0]);
 
         // Find the TextView in the list_item.xml layout with the ID location.
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
+        TextView locationTextView = (TextView) listItemView.findViewById(R.id.primary_location);
         // Get the location from the formatLocation method and set this text on
         // the TextView.
         // 1 index is a primary location string
@@ -90,6 +102,45 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
 
         return listItemView;
+    }
+
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 
     private String formatMagnitude(double magnitude) {
